c4fb402a6f383e46bd507265ebd110658c8843b2
Todd Geist <todd@geistinteractive.com>
2019-12-25 22:55:16 -0500

    add loader

---
+++ template/src/Widget.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function Widget(initialProps) {
+  return <div>{initialProps.message}</div>;
+}
+
+export default Widget;
+git a
--- template/src/index.js
+++ template/src/index.js
@@ -3,6 +3,26 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Widget from "./Widget";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function BootWidget(props) {
+  ReactDOM.render(<Widget {...props} />, document.getElementById("root"));
+}
+
+// if we have merged in initialProps use them to boot the widget
+if (window.__initialProps__ !== "__PROPS__") {
+  try {
+    window.__initialProps__ = JSON.parse(window.__initialProps__);
+  } catch (error) {}
+  BootWidget(window.__initialProps);
+} else {
+  // we haven't merged so install loadInitialProps method for FM to use
+  window.loadInitialProps = function(props) {
+    // boot the widget with those props
+    BootWidget(props);
+  };
+
+  //comment this step to prevent loading with default props
+  // once this is commented out, react won't even render until props are found.
+  BootWidget({ message: "Default props are loaded" });
+}
