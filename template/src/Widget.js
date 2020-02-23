133e0c6a875e435163ac7a3acb8d7d79a3c30d39
Todd Geist <todd@geistinteractive.com>
2020-02-22 19:11:16 -0800

    added find example. updated FM file

--- template/src/Widget.js
+++ template/src/Widget.js
@@ -1,23 +1,40 @@
 import React from "react";
 import FMScriptButton from "./components/FMScriptButton";
-import { useFMPerformJS, useFMScript } from "fmw-react-hooks";
+import { useFMPerformJS, useFMFindScript } from "fmw-react-hooks";
 
 function Widget(initialProps) {
   const messageColor = useFMPerformJS("blue", "changeColor");
-  const { data } = useFMScript("getvalue", null);
+
+  const { data, error } = useFMFindScript("AddonNameFind", {
+    layouts: "AddonSampleData",
+    query: [{ PrimaryKey: "*" }]
+  });
+
+  if (error) {
+    console.error(error);
+  }
+  if (!data) return "";
+
   const style = { color: messageColor };
 
   return (
     <>
+      <h1>Test Addon</h1>
       <p style={style}>{initialProps.message}</p>
       <FMScriptButton
-        scriptName="HandleOnClick"
+        scriptName="AddonNameHandleOnClick"
         scriptParameter="Hi! I am from the web viewer"
       >
         Run a FileMaker Script
       </FMScriptButton>
       <hr />
-      {data}
+      <h2>Fetched From FileMaker</h2>
+      <ul>
+        {data.map(record => {
+          const name = record.fieldData.Name;
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
     </>
   );
 }
